7cceb6c58ab0abf38aef0da460ecb2aff51f8118
lead0lead <159436295+lead0lead@users.noreply.github.com>
2024-09-17 20:12:05 +0200

    Rework guess input, add method to check guesses

--- main.rb
+++ main.rb
@@ -7,24 +7,32 @@ code_length = 4
 def generate_secret_code(color_amount, code_length)
   secret_code = []
   1.upto(code_length) do
-    secret_code.push(rand(color_amount) + 1)
+    secret_code.push((rand(color_amount) + 1).to_s)
   end
   secret_code
 end
 
-def make_guess(secret_code, guessed_code)
-  if guessed_code.split("").each { |i| i.to_i > 0 && i.to_i <= 6}
-    'Valid Input'
-  else
-    'Invalid Input'
-  end
+def make_guess
+  puts 'Please Enter Code'
+  gets.chomp.split('')
 end
 
-puts 'pleaser enter code'
-guessed_code = gets
-secret_code = generate_secret_code(color_amount, code_length)
+def check_guess(guessed_code, secret_code)
+  response = []
+  guessed_code.each_with_index do |value, index|
+    if secret_code.include?(value) && secret_code.find_index(value) == index
+      response.push('Black')
+    elsif secret_code.include?(value) && secret_code.find_index(value) != index
+      response.push('White')
+    else
+      response.push('Empty')
+    end
+  end
+  response
+end
 
-guess = make_guess(secret_code, guessed_code)
-
-puts secret_code.join
-puts guess
+secret_code = generate_secret_code(6, 4)
+p secret_code
+guessed_code = make_guess
+p guessed_code
+p check_guess(guessed_code, secret_code)
