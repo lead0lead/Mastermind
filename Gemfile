b043aa82a2ab7d86a1beab46dffbd97ef8dfcd4b
lead0lead <159436295+lead0lead@users.noreply.github.com>
2024-09-20 12:17:48 +0200

    Restructure code
    add classes

--- Gemfile
+++ Gemfile
@@ -5,3 +5,5 @@ source "https://rubygems.org"
 # gem "rails"
 
 gem "rubocop", "~> 1.66"
+
+gem "ruby-lsp", "~> 0.18.0"
--- main.rb
+++ main.rb
@@ -1,38 +1,53 @@
 # frozen_string_literal: true
 
-color_amount = 6
+# Responsible for all game logic
+class Game
+  attr_reader :color_amount, :code_length
 
-code_length = 4
-
-def generate_secret_code(color_amount, code_length)
-  secret_code = []
-  1.upto(code_length) do
-    secret_code.push((rand(color_amount) + 1).to_s)
+  def initialize(color_amount, code_length)
+    @color_amount = color_amount
+    @code_length = code_length
   end
-  secret_code
-end
 
-def make_guess
-  puts 'Please Enter a Code in the following format: 1234'
-  gets.chomp.split('')
-end
-
-def check_guess(guessed_code, secret_code)
-  response = []
-  guessed_code.each_with_index do |value, index|
-    if secret_code.include?(value) && secret_code[index] == value
-      response.push('Black')
-    elsif secret_code.include?(value) && secret_code[index] != value
-      response.push('White')
-    else
-      response.push('Empty')
+  def generate_secret_code(color_amount, code_length)
+    secret_code = []
+    1.upto(code_length) do
+      secret_code.push((rand(color_amount) + 1).to_s)
     end
+    secret_code
   end
-  response.shuffle
+
+  def make_guess
+    puts 'Please Enter a Code in the following format: 1234'
+    gets.chomp.split('')
+  end
+
+  def check_guess(guessed_code, secret_code)
+    response = []
+    guessed_code.each_with_index do |value, index|
+      if secret_code.include?(value) && secret_code[index] == value
+        response.push('Black')
+      elsif secret_code.include?(value) && secret_code[index] != value
+        response.push('White')
+      else
+        response.push('Empty')
+      end
+    end
+    response.shuffle
+  end
+
+  def check_round_result(checked_code_response)
+    return 'Wrong Code' unless checked_code_response.each { |i| i == 'Black' }
+
+    'Correct Code!'
+  end
 end
 
-secret_code = generate_secret_code(color_amount, code_length)
+game = Game.new(6, 4)
+
+secret_code = game.generate_secret_code(game.color_amount, game.code_length)
 p secret_code
-guessed_code = make_guess
+guessed_code = game.make_guess
 p guessed_code
-p check_guess(guessed_code, secret_code)
+checked_code_response = game.check_guess(guessed_code, secret_code)
+p game.check_round_result(checked_code_response)
