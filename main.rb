f50aae50d69ade951a730c5e26714f0725bf871a
lead0lead <159436295+lead0lead@users.noreply.github.com>
2024-09-20 15:54:00 +0200

    Add rounds system to play full matches

--- main.rb
+++ main.rb
@@ -43,10 +43,19 @@ class Game
   end
 end
 
-game = Game.new(6, 4)
-secret_code = game.generate_secret_code(game.color_amount, game.code_length)
-p secret_code
-guessed_code = game.make_guess
-p guessed_code
-checked_code_response = game.check_guess(guessed_code, secret_code)
-p game.check_round_result(checked_code_response)
+def play_game(color_amount, code_length, max_rounds)
+  game = Game.new(color_amount, code_length)
+  secret_code = game.generate_secret_code(game.color_amount, game.code_length)
+  p secret_code
+  1.upto(max_rounds) do |round|
+    puts "Now playing round: #{round}"
+    guessed_code = game.make_guess
+    checked_code_response = game.check_guess(guessed_code, secret_code)
+    return "Game Won! in round #{round}" if game.check_round_result(checked_code_response) == 'Correct Code!'
+
+    puts "You made your guess: The response is: #{checked_code_response}"
+  end
+  "Game lost! The correct code was #{secret_code}"
+end
+
+p play_game(6, 4, 12)
