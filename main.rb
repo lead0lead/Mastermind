2ac5fff6a1f225452f9e36ed1c11c350bb570843
lead0lead <159436295+lead0lead@users.noreply.github.com>
2024-09-20 15:58:56 +0200

    Restructure file
    Seperate project into multiple files

--- main.rb
+++ main.rb
@@ -1,47 +1,6 @@
 # frozen_string_literal: true
 
-# Responsible for all game logic
-class Game
-  attr_reader :color_amount, :code_length
-
-  def initialize(color_amount, code_length)
-    @color_amount = color_amount
-    @code_length = code_length
-  end
-
-  def generate_secret_code(color_amount, code_length)
-    secret_code = []
-    1.upto(code_length) do
-      secret_code.push((rand(color_amount) + 1).to_s)
-    end
-    secret_code
-  end
-
-  def make_guess
-    puts 'Please Enter a Code in the following format: 1234'
-    gets.chomp.split('')
-  end
-
-  def check_guess(guessed_code, secret_code)
-    response = []
-    guessed_code.each_with_index do |value, index|
-      if secret_code.include?(value) && secret_code[index] == value
-        response.push('Black')
-      elsif secret_code.include?(value) && secret_code[index] != value
-        response.push('White')
-      else
-        response.push('Empty')
-      end
-    end
-    response.shuffle
-  end
-
-  def check_round_result(checked_code_response)
-    return 'Wrong Code' unless checked_code_response.uniq.size == 1
-
-    'Correct Code!'
-  end
-end
+require_relative 'lib/game'
 
 def play_game(color_amount, code_length, max_rounds)
   game = Game.new(color_amount, code_length)
